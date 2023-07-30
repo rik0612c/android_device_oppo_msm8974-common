cb5be240c19151ac7d2d7d13ca408c860930c181
rik0612c <qq872638399@gmail.com>
2023-07-30 21:28:31 +0800

    msm8974-common: Fix DRM clearkey

--- msm8974.mk
+++ msm8974.mk
@@ -84,9 +84,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.4-service.clearkey
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
