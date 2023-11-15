11be32a7f9552ffc6c3a8c9d13f2590c92bbeffe
khalvat-M <mmba313@gmail.com>
2023-11-15 16:04:33 +0330

    msm8974-common: Set OVERRIDE_TARGET_FLATTEN_APEX := true

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -20,6 +20,6 @@ LOCAL_SRC_FILES := libaudioclient_shim.cpp
 LOCAL_MODULE := libaudioclient_shim
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_SHARED_LIBRARIES := libaudioclient
-LOCAL_VENDOR_MODULE := true
+LOCAL_VENDOR_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
